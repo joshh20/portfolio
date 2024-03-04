086b7cb66296da3b1cd6b86c8ea28e385a728652
joshh20 <joshh20@users.noreply.github.com>
2024-03-04 13:36:01 -0500

    Added React Hook Form to make managing form easier. Made some style changes and implemented form in ContactForm.jsx. Have yet to hook up the form to EmailJS.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,83 +1,114 @@
-import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
 
 export default function ContactForm() {
-    const form = useRef();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
+    console.log(errors);
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs
-            .sendForm("service_mvp2n1t", "template_exyd9lo", form.current, {
-                publicKey: "M33VdhyGoy6FOroWy",
-                blockHeadless: true,
-                limitRate: {
-                    throttle: 5000,
-                },
-            })
-            .then(
-                () => {
-                    console.log("SUCCESS!");
-                },
-                (error) => {
-                    console.log("FAILED...", error.text);
-                },
-            );
-    };
+    function formatErrors(errorMessage) {
+        return (
+            <p
+                role="alert"
+                className="font-semibold text-red-500 dark:font-normal"
+            >
+                {errorMessage}
+            </p>
+        );
+    }
 
     return (
-        <div className="flex items-center justify-center">
-            <form
-                ref={form}
-                onSubmit={sendEmail}
-                className="w-full max-w-2xl rounded-lg bg-slate-200/10 p-8 shadow-lg transition duration-500 ease-in-out dark:bg-slate-900"
-            >
-                <div className="mb-6">
-                    <label className="font-medium text-slate-800 dark:text-slate-300 sm:text-lg">
-                        First Name
-                    </label>
-                    <input
-                        type="text"
-                        name="firstName"
-                        className="mt-2 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-gray-500"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="font-medium text-slate-800 dark:text-slate-300 sm:text-lg">
-                        Last Name
-                    </label>
-                    <input
-                        type="text"
-                        name="lastName"
-                        className="mt-2 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-gray-500"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="font-medium text-slate-800 dark:text-slate-300 sm:text-lg">
-                        Email
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        className="mt-2 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-gray-500"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label className="font-medium text-slate-800 dark:text-slate-300 sm:text-lg">
-                        Message
-                    </label>
-                    <textarea
-                        name="message"
-                        rows="4"
-                        className="mt-2 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-gray-500"
-                    ></textarea>
-                </div>
-                <input
-                    type="submit"
-                    value="Send"
-                    className="w-full cursor-pointer rounded-md bg-blue-700 px-4 py-2 font-medium text-white transition-all hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 dark:border-gray-700 dark:hover:ring-offset-2 sm:text-lg"
-                />
-            </form>
-        </div>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto max-w-2xl rounded-lg bg-slate-300/20 p-8 shadow-lg transition duration-500 ease-in-out dark:bg-slate-900"
+        >
+            {/* First name */}
+            <input
+                type="text"
+                placeholder="First name"
+                className={`my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${
+                    errors.firstName
+                        ? "border border-red-500 focus:border-red-700 dark:border-red-500 dark:focus:border-red-700"
+                        : ""
+                }`}
+                {...register("firstName", {
+                    required: true,
+                    maxLength: 80,
+                })}
+                aria-invalid={errors.firstName ? "true" : "false"}
+            />
+            {/* First name error messages */}
+            {errors.firstName?.type === "required" &&
+                formatErrors("First name is required")}
+            {errors.firstName?.type === "maxLength" &&
+                formatErrors("Please don't exceed 80 characters")}
+            {/* Last name */}
+            <input
+                type="text"
+                placeholder="Last name"
+                className={`my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${
+                    errors.lastName
+                        ? "border border-red-500 focus:border-red-700 dark:border-red-500 dark:focus:border-red-700"
+                        : ""
+                }`}
+                {...register("lastName", {
+                    required: true,
+                    maxLength: 80,
+                })}
+                aria-invalid={errors.lastName ? "true" : "false"}
+            />
+            {/* Last name error messages */}
+            {errors.lastName?.type === "required" &&
+                formatErrors("Last name is required")}
+            {errors.lastName?.type === "maxLength" &&
+                formatErrors("Please don't exceed 80 characters")}
+            {/* Email */}
+            <input
+                type="email"
+                placeholder="Email"
+                className={`my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${
+                    errors.email
+                        ? "border border-red-500 focus:border-red-700 dark:border-red-500 dark:focus:border-red-700"
+                        : ""
+                }`}
+                {...register("email", {
+                    required: true,
+                    pattern:
+                        /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i,
+                })}
+                aria-invalid={errors.email ? "true" : "false"}
+            />
+            {/* Email error messages */}
+            {errors.email?.type === "required" &&
+                formatErrors("Please enter your email address")}
+            {errors.email?.type === "pattern" &&
+                formatErrors("Please make sure to enter a valid email address")}
+            {/* Message */}
+            <textarea
+                {...register("message", {
+                    required: true,
+                })}
+                rows={5}
+                placeholder="Message"
+                className={`my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${
+                    errors.message
+                        ? "border border-red-500 focus:border-red-700 dark:border-red-500 dark:focus:border-red-700"
+                        : ""
+                }`}
+                aria-invalid={errors.message ? "true" : "false"}
+            />
+            {/* Message box error messages */}
+            {errors.message?.type === "required" &&
+                formatErrors("Please share your thoughts 😁")}
+            {/* Submit */}
+            <input
+                type="submit"
+                className="mt-5 w-full cursor-pointer rounded-md bg-blue-700 px-4 py-2 font-medium text-white transition-all hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 dark:border-gray-700 dark:hover:ring-offset-2 sm:text-lg"
+            />
+        </form>
     );
 }
