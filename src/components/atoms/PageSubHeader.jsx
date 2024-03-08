86a45cfedf1603d76756b08a827bd63497d5f197
joshh20 <joshh20@users.noreply.github.com>
2024-03-08 17:28:18 -0500

    Changed Contact page subheader text. Updated PageSubHeader font size.

--- src/components/atoms/PageSubHeader.jsx
+++ src/components/atoms/PageSubHeader.jsx
@@ -1,6 +1,6 @@
 export default function PageSubHeader({ text }) {
     return (
-        <h2 className="mb-4 text-lg font-bold text-slate-800 duration-500 dark:text-slate-300 sm:text-xl">
+        <h2 className="mb-4 font-semibold text-slate-800 duration-500 dark:text-slate-300 sm:text-lg">
             {text}
         </h2>
     );
