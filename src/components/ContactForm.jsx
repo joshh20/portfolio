57c1cad09d942c5127c58c093c9bde962ce5d998
joshh20 <joshh20@users.noreply.github.com>
2024-03-08 15:59:04 -0500

    Made some styling changes in ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -42,13 +42,13 @@ export default function ContactForm() {
     };
 
     function formatInputElement(errors, registerName) {
-        return `my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${errors[registerName] ? "ring-1 ring-red-500 focus:ring-2 focus:ring-red-700 dark:focus:ring" : ""}`;
+        return `my-3 w-full rounded-md border border-gray-300 px-3 py-2 placeholder-slate-400 shadow-sm focus:border-blue-700 focus:outline-none focus:ring-blue-700 dark:focus:ring-1 dark:focus:ring-gray-500 dark:border-gray-700 dark:bg-slate-800 dark:text-slate-300 dark:placeholder-slate-500 ${errors[registerName] ? "ring-1 ring-red-500 focus:ring-2 focus:ring-red-700 dark:focus:ring-red-700 dark:focus:ring-[3px]" : ""}`;
     }
 
     return (
         <form
             onSubmit={handleSubmit(onSubmit)}
-            className="mx-auto max-w-2xl rounded-lg bg-slate-300/20 p-4 shadow-lg transition duration-500 ease-in-out dark:bg-slate-900 sm:p-8"
+            className="mx-auto max-w-2xl rounded-lg bg-slate-300/20 p-4 shadow-md transition duration-500 ease-in-out dark:bg-slate-900 sm:p-8"
         >
             {/* First name */}
             <input
