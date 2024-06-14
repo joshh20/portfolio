ee37bdc1bffbdcb0bdbb76288f6a0c1a0f3f1837
joshh20 <joshh20@users.noreply.github.com>
2024-06-14 15:12:35 -0400

    Initial work on porting Contact page

--- components/atoms/PageHeader.tsx
+++ components/atoms/PageHeader.tsx
@@ -1,4 +1,4 @@
-export default function PageHeader({ title }) {
+export default function PageHeader({ title }: { title: string }) {
     return (
         <h1 className="mb-4 text-3xl font-bold text-slate-800 duration-500 dark:text-slate-300 sm:text-4xl">
             {title}
--- components/atoms/PageSubHeader.tsx
+++ components/atoms/PageSubHeader.tsx
@@ -1,4 +1,4 @@
-export default function PageSubHeader({ text }) {
+export default function PageSubHeader({ text }: { text: string }) {
     return (
         <h2 className="mb-4 font-semibold text-slate-800 duration-500 dark:text-slate-300 sm:text-lg">
             {text}
