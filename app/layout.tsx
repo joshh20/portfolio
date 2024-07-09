d5be5a77730d40453268606dadd4cb21a57b2cc4
joshh20 <joshh20@users.noreply.github.com>
2024-07-09 15:28:32 -0400

    Added overflow-y-scroll to the root html styling so that the y scrollbar doesn't shift the page content when it appears.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning className="overflow-y-scroll">
             <body className={nunito.className}>
                 <ThemeProvider attribute="class">
                     <Header />
