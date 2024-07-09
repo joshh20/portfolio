ee5461618d272cd7701133a7e5b1fa57ed522cb2
joshh20 <joshh20@users.noreply.github.com>
2024-07-08 21:01:51 -0400

    Temporarily disabled Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,22 @@
-import { configData } from "@/assets/configData";
-import ContactForm from "@/components/ContactForm";
-import PageHeader from "@/components/atoms/PageHeader";
-import PageSubHeader from "@/components/atoms/PageSubHeader";
+// import { configData } from "@/assets/configData";
+// import ContactForm from "@/components/ContactForm";
+// import PageHeader from "@/components/atoms/PageHeader";
+// import PageSubHeader from "@/components/atoms/PageSubHeader";
 
-export const metadata = {
-    title: "Projects",
-};
+// export const metadata = {
+//     title: "Projects",
+// };
+
+// export default function Contact() {
+//     return (
+//         <section className="mx-auto max-w-2xl">
+//             <PageHeader title={configData.pages.contactPage.header} />
+//             <PageSubHeader text={configData.pages.contactPage.subheader} />
+//             <ContactForm />
+//         </section>
+//     );
+// }
 
 export default function Contact() {
-    return (
-        <section className="mx-auto max-w-2xl">
-            <PageHeader title={configData.pages.contactPage.header} />
-            <PageSubHeader text={configData.pages.contactPage.subheader} />
-            <ContactForm />
-        </section>
-    );
+    return <h1>Coming soon</h1>;
 }
