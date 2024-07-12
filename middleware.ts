207783c41964e6d6904dacfac09d9ec911ef8784
joshh20 <joshh20@users.noreply.github.com>
2024-07-12 18:00:15 -0400

    Created some basic access logging for the app using NextJS middleware.

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import getIPGeoLocation from "@/utils/getIPGeoLocation";
+
+export async function middleware(request: NextRequest) {
+    const ip = request.headers.get("CF-Connecting-IP");
+    const country = request.headers.get("CF-IPCountry");
+    const url = JSON.stringify(request.url);
+    const userAgent = JSON.stringify(request.headers.get("user-agent"));
+    const statusCode = NextResponse.next().status;
+    const method = request.method;
+
+    // let location;
+    // try {
+    //     location = await getIPGeoLocation("24.112.145.126");
+    // } catch (error) {
+    //     console.error("Error fetching location: ", error);
+    // }
+
+    console.log(
+        `${method} ${statusCode} ${url} User Agent:${userAgent} IP:${ip} Country:${country} \n`
+    );
+}
